0e8215577999178e39e09e12c220ee244df2929e
Miralik <mirrahimovmiralim7@gmail.com>
2023-04-28 19:01:26 +0600

    addedModelOfAnimal

---
+++ Models/Animal.cs
@@ -0,0 +1,6 @@
+﻿namespace home_work_49.Models
+{
+    public class Animal
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace home_work_49.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
